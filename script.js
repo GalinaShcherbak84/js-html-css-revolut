ce8c0f7c32720fccfd82ce517f4c7c710b20a4e4
GalinaSherbak84 <galinasherbak@gmail.com>
2021-04-09 22:13:46 +0200

    script

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
-//console.log('jquery', jQuery)
-
+//console.log('jquery', jQuery);
+var dropDown = $('.dropdown');
+var dropLinks = dropDown.children('a');
+//console.log(dropLinks);
+var dropMenus = dropDown.children('ul');
+//console.log(dropMenus);
+dropLinks.click(function(){
+    var actualMenu = $(this).next('ul');
+    dropMenus.not(actualMenu).hide();
+    actualMenu.toggle();
+})
 
 
     
